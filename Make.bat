458306ee617db0f3355c4f835d4a1c4c1b1d88a6
Andrew Jackson <andrew.d.jackson@gmail.com>
2020-07-29 20:59:30 +0100

    version number

--- Make.bat
+++ Make.bat
@@ -4,7 +4,7 @@ set DISTPATH=dist
 set RESOURCESPATH=resources
 set WINDOWSDISTPATH=dist/windows
 set WINDOWS=%WINDOWSDISTPATH%/%EXECUTABLE%.exe
-set VERSION=1.0.1
+set VERSION=1.5.3
 set VCNEXE="C:\Program Files\CodeNotary\vcn"
 
 go build -i -v -o %WINDOWS% -ldflags="-H windowsgui -s -w -X main.version=%VERSION%"
