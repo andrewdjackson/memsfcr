5ba479e97fc4b873029ffde0cd8422850c67e7b8
Andrew Jackson <andrew.d.jackson@gmail.com>
2021-09-15 15:40:07 +0100

    increased version

--- Make.bat
+++ Make.bat
@@ -4,7 +4,7 @@ set DISTPATH=dist
 set RESOURCESPATH=resources
 set WINDOWSDISTPATH=dist/windows
 set WINDOWS=%WINDOWSDISTPATH%/%EXECUTABLE%.exe
-set VERSION=2.3.5
+set VERSION=2.3.8
 set VCNEXE="C:\Program Files\CodeNotary\vcn"
 
 go build -i -v -o %WINDOWS% -ldflags="-H windowsgui -s -w -X main.version=%VERSION%"
