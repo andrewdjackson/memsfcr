daecbcdf116e45bbfa9d076d51f4e07b20c78914
andrewdjackson <andrew.d.jackson@gmail.com>
2021-09-15 16:00:33 +0100

    update make.bat

--- Make.bat
+++ Make.bat
@@ -5,10 +5,5 @@ set RESOURCESPATH=resources
 set WINDOWSDISTPATH=dist/windows
 set WINDOWS=%WINDOWSDISTPATH%/%EXECUTABLE%.exe
 set VERSION=2.3.8
-set VCNEXE="C:\Program Files\CodeNotary\vcn"
 
-go build -i -v -o %WINDOWS% -ldflags="-H windowsgui -s -w -X main.version=%VERSION%"
-
-rem %VCNEXE% notarize %WINDOWS%
-rem %VCNEXE% login
-rem %VCNEXE% authenticate %WINDOWS%
+go build -o %WINDOWS% -ldflags="-H windowsgui -s -w -X main.version=%VERSION%"
