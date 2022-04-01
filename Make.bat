3a591ef7c12af3d4035b78a6966005f40b4f1775
Andrew Jackson <andrew.d.jackson@gmail.com>
2022-04-01 17:14:16 +0100

    version 2.5.0

--- Make.bat
+++ Make.bat
@@ -4,6 +4,6 @@ set DISTPATH=dist
 set RESOURCESPATH=resources
 set WINDOWSDISTPATH=dist/windows
 set WINDOWS=%WINDOWSDISTPATH%/%EXECUTABLE%.exe
-set VERSION=2.4.1
+set VERSION=2.5.0
 
 go build -o %WINDOWS% -ldflags="-H windowsgui -s -w -X main.version=%VERSION%"
